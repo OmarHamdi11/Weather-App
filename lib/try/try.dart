651f912e27788365dc551a33fe8675046005404d
OmarHamd11 <omarellafy1@gmail.com>
2024-07-01 19:05:13 +0300

    upload try

---
+++ lib/try/try.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Try extends StatelessWidget {
+  const Try({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('hello'),
+      ),
+    );
+  }
+}
